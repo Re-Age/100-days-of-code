f759885ef956f973f882a7b2c7654b06fefb3413
Re-Age <namiki.dev@gmail.com>
2020-10-01 00:34:57 +0900

    Kotlin official tutorial "Functions" #2 at 3-6

--- Prac1_IDEA/src/main/kotlin/practice/function/lesson2.kt
+++ Prac1_IDEA/src/main/kotlin/practice/function/lesson2.kt
@@ -1,5 +1,7 @@
 package main.kotlin.practice.function
 
+import javax.print.attribute.standard.MediaSize
+
 fun main(){
     // 1)
     infix fun Int.times(str: String) = str.repeat(this)
@@ -14,4 +16,25 @@ fun main(){
     // 2'')
     println(2.times("Bye"))
 
+    // 3)
+    var pair = "Ferrari" to "Katarina"
+    println(pair)
+    //  (Ferrari, Katarina)
+
+    // 4)
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+    // (McLaren, Lucas)
+
+    // 5)
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    sophia likes claudia
+}
+
+// 6)
+class Person(val name: String){
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) {likedPeople.add(other)}
 }
